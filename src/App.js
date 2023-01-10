bfafcc5324de75668920c0f221bb92608ea5b1c8
Chokri Ben Hassen <chokribenhassen48@gmail.com>
2023-01-10 17:35:14 +0100

    add the navbar component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 
 
-
+import Navbar from "./components/Navbar";
 function App() {
   return (
-   <div className="container">
+   <div>
+   <Navbar />
+    <div className="container">
     <h1> React App</h1>
     <button class="btn btn-danger">ok</button>
+    </div>
+    
    </div>
   );
 }
